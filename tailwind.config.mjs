2c59b9aa5c6286133ab5cfade71e40150e062405
Jonathan Rodriguez <jonathan.rdzes@gmail.com>
2024-09-28 17:09:59 -0600

    Added configurations and fonts

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"space-navy": "#0B0D17",
+			"soft-blue": "#D0D6F9",
+			"white": "#FFFFFF",
+		},
+		extend: {
+			fontFamily: {
+				"sans": ["Barlow Condensed", ...defaultTheme.fontFamily.sans],
+				"serif": ["Bellefair", ...defaultTheme.fontFamily.serif],
+				"body": ["Barlow"],
+			}
+		}
+	},
+	plugins: [],
+}
